5363fb5ca1dfcb7444b9d71085b5c579b9a6c5ad
python240419 <51232248+python240419@users.noreply.github.com>
2019-08-30 12:56:59 +0300

    Update main.py

--- ex_solution/main.py
+++ ex_solution/main.py
@@ -1,9 +1,11 @@
 from directoryapi import *
 
-dc = DirectoryCreator('c:/itay')
-while True:
-    name = input("folder name? ")
-    if dc.safeCreateFolder(name):
-        break
+def main():
+    dc = DirectoryCreator('c:/itay')
+    while True:
+        name = input("folder name? ")
+        if dc.safeCreateFolder(name):
+            break
 
-print("Success!")
+    print("Success!")
+main()

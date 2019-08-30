5dfaaac486b068be42d3f5817dd5aa67a283fc3b
python240419 <51232248+python240419@users.noreply.github.com>
2019-08-30 11:41:14 +0300

    Create inputapi.py

---
+++ inputapi.py
@@ -0,0 +1,28 @@
+class SmartInput():
+    def __init__(self):
+        pass
+    def __str__(self):
+        return 'methods: inputInt'
+    def inputInt(self, msg):
+        '''
+        Get an int number from the user
+        input number till valid
+        int was receieved
+        :return: int number
+        '''
+        while True:
+            try:
+                n = int(input(msg))
+                return n
+            except: # catch
+                print("error, try again")
+    def positiveFloat(self, msg):
+        '''
+        Get a float number from
+        the user
+        input number till valid
+        float was receieved
+        :param msg: show message
+        :return: float positive
+        '''
+        pass
